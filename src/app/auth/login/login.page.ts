1c3f039cf5644ffe77bfe8d991b272596fdee515
Siyu Xiang <siyuxiang@siyudeair.home>
2019-09-06 01:46:49 -0700

    add birthdate validator

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/service/auth.service';
+import { UsernameValidator } from '../../validators/username.validator';
+import { BirthdateValidator } from '../../validators/birthdate.validator';
+import {Validators, FormGroup, FormControl, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,15 +15,32 @@ export class LoginPage implements OnInit {
    * An object representing the user for the login form
    */
   public user: any;
+  validations_form: FormGroup;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,
+    public formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
     this.user = {
       email: '',
       password: '',
       year_of_birth: ''
     };
+
+    this.validations_form = this.formBuilder.group({
+      username: new FormControl('', Validators.compose([
+        UsernameValidator.validUsername,
+        Validators.maxLength(25),
+        Validators.minLength(5),
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+        Validators.required
+      ])),
+      birthdate: new FormControl('', Validators.compose([
+        BirthdateValidator.validUsDate,
+        Validators.required
+      ]))
+    });
   }
 
   login() {
@@ -38,5 +58,20 @@ export class LoginPage implements OnInit {
   logout() {
     this.authService.logout();
   }
+
+    // validation message when input information is incorrect
+    validation_messages = {
+      'username': [
+        { type: 'required', message: 'Username is required.' },
+        { type: 'minlength', message: 'Username must be at least 5 characters long.' },
+        { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
+        { type: 'pattern', message: 'Please enter a valid email.' },
+        { type: 'validUsername', message: 'Your username has already been taken.' }
+      ],
+      'birthdate': [
+        { type: 'required', message: 'Birthdate is required.' },
+        { type: 'validUsDate', message: 'Please enter birthdate with format: MM/DD/YYYY'},
+      ]
+    };
 
 }
