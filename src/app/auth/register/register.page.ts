adbcdffb3db5c3bca719c6299b3f9c5dd8ed8a3a
Siyu Xiang <siyuxiang@siyudeair.home>
2019-10-07 01:35:25 -0700

    initialize

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -63,6 +63,7 @@ export class RegisterPage implements OnInit {
   }
 
   register() {
+    this.changeDateFormat;
     this.authService.register({
       user: {
         email: this.validations_form.get('username').value
@@ -80,6 +81,12 @@ export class RegisterPage implements OnInit {
         }
       }
     );
+  }
+
+  changeDateFormat() {
+    var date = new Date(this.validations_form.get("birthdate").value);
+    this.validations_form.get("birthdate").setValue(date.getFullYear.toString + 
+      "-" + date.getMonth.toString + "-" + date.getDay.toString);
   }
 
   onSubmit(values) {
