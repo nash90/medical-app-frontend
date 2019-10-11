44e9d7b85560410b4004f1d3d460854b28d44c45
Abinash Adhikari (nash90) <nash.rakuten@gmail.com>
2019-10-11 12:05:18 -0700

    updated menu section

--- src/app/pages/help/help.page.ts
+++ src/app/pages/help/help.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-help',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
+  goToMenu() {
+    this.navCtrl.navigateRoot('/menu');
+  }
 }
